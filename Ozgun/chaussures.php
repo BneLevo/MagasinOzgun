44245f8aea629b6f045fd221fe925ae6d189114e
BneLevo <levent.ozgn@eduge.ch>
2025-04-14 16:09:09 +0200

    avancer le projet

---
+++ Ozgun/chaussures.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body id="pageChaussures">
+    <?php include("header.php") ?>
+
+    <main>
+        <div>
+            <section id="imgEtChaussure">
+                <div id="imageChaussure"></div>
+                <div id="couleurChaussure"></div>
+            </section>
+    
+            <section id="Commentaires">
+                
+            </section>
+        </div>
+
+        <section id="infoChaussures">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
+    </main>
+</body>
+
+</html>
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'database.php';
-
-$d = new Database;
-
-$d->connect();
